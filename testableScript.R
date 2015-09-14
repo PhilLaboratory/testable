96a7054327370d0d2d04c10be2a822e68a8ecf8b
Jonathan  Scott  Phillips <phillips01@g.harvard.edu>
2015-09-13 23:39:29 -0400

    add testable data import script
    Adding an R script to combine all of the individual participant files
    into a single .csv.

---
+++ testableScript.R
@@ -0,0 +1,56 @@
+#### Info: #####
+## For questions, or problems, email me or create a github branch with the proposed changes at 
+## email: phillips01@g.harvard.edu
+## github: https://github.com/phillipsjs/testable
+
+
+#### start script #####
+
+# This is a script to take the individual participant files from a testable study and merge them into a single file
+# make sure there are NO files other than the testable data files in the folder!!
+
+## clear out the environment
+rm(list=ls()) 
+
+## set the working directory to the folder where the testable data has been downloaded
+setwd("C:/Users/Jphil/Desktop/results_files") 
+
+files <- list.files()
+for(i in 1:length(files)) assign(files[i], read.csv(files[i],header=F,stringsAsFactors=F))
+
+colNum <- length(get(files[1]))
+
+d <- data.frame(matrix(nrow=0,ncol=colNum))
+colnames(d) <- get(files[1])[3,]
+
+for(i in 1:length(files)){
+   j <- get(files[i])
+   
+   j$turkID <- j[2,1]         # get Mturk ID
+   j$age <- j[2,2]            # get age
+   j$sex <- j[2,3]            # get sex
+   j$education <- j[2,4]      # get education
+   j$handedness <- j[2,5]     # get handedness
+   j$other1 <- j[2,6]         # get other 1
+   j$other2 <- j[2,7]         # get other 2
+   j$other3 <- j[2,8]         # get other 3
+   j$other4 <- j[2,9]         # get other 4
+   j$other5 <- j[2,10]        # get other 5
+   j$browser <- j[2,11]       # get browser
+   j$version <- j[2,12]       # get version
+   j$screenWidth <- j[2,13]   # get screen width
+   j$screenHeight <- j[2,14]  # get screen height
+   j$OS <- j[2,15]            # get operating system
+   j$OS_lang <- j[2,16]       # get operating system language
+   j$calibration <- j[2,17]   # get calibration
+   j$ip <- j[2,18]            # get IP address
+   j$code <- j[2,19]          # get completion code
+
+   j <- j[-c(1:3),]
+   d <- rbind(d,j)
+   }
+
+colnames(d) <- c(get(files[1])[3,],colnames(d[,21:39]))
+
+setwd("../")
+write.csv(d,file="data.csv", row.names=F)
