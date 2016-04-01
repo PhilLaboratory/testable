b135f943a9418c47fe25bab93c0e9e0589233f50
Jonathan  Scott  Phillips <phillips01@g.harvard.edu>
2016-04-01 14:03:33 -0400

    update R script for new demo fields

--- testableScript.R
+++ testableScript.R
@@ -13,7 +13,7 @@
 rm(list=ls()) 
 
 ## set the working directory to the folder where the testable data has been downloaded
-setwd("C:/Users/Jphil/Desktop/results_files") 
+setwd("C:/Users/Jphil/Documents/currentProjects/Knowledge and Belief/testable/frenchPilot") 
 
 files <- list.files()
 for(i in 1:length(files)) assign(files[i], read.csv(files[i],header=F,stringsAsFactors=F))
@@ -21,7 +21,7 @@ for(i in 1:length(files)) assign(files[i], read.csv(files[i],header=F,stringsAsF
 colNum <- length(get(files[1]))
 
 d <- data.frame(matrix(nrow=0,ncol=colNum))
-colnames(d) <- get(files[1])[3,]
+colnames(d) <- get(files[1])[4,]
 
 for(i in 1:length(files)){
    j <- get(files[i])
@@ -31,26 +31,29 @@ for(i in 1:length(files)){
    j$sex <- j[2,3]            # get sex
    j$education <- j[2,4]      # get education
    j$handedness <- j[2,5]     # get handedness
-   j$other1 <- j[2,6]         # get other 1
-   j$other2 <- j[2,7]         # get other 2
-   j$other3 <- j[2,8]         # get other 3
-   j$other4 <- j[2,9]         # get other 4
-   j$other5 <- j[2,10]        # get other 5
-   j$browser <- j[2,11]       # get browser
-   j$version <- j[2,12]       # get version
-   j$screenWidth <- j[2,13]   # get screen width
-   j$screenHeight <- j[2,14]  # get screen height
-   j$OS <- j[2,15]            # get operating system
-   j$OS_lang <- j[2,16]       # get operating system language
-   j$calibration <- j[2,17]   # get calibration
-   j$ip <- j[2,18]            # get IP address
-   j$code <- j[2,19]          # get completion code
+   j$sexOrientation <- j[2,6] # get sexual orientation
+   j$ethnicity <- j[2,7]      # get ethnicity
+   j$ethnicity <- j[2,8]      # get nationality
+   j$other1 <- j[2,9]         # get other 1
+   j$other2 <- j[2,10]         # get other 2
+   j$other3 <- j[2,11]         # get other 3
+   j$other4 <- j[2,12]         # get other 4
+   j$other5 <- j[2,13]        # get other 5
+   j$browser <- j[2,14]       # get browser
+   j$version <- j[2,15]       # get version
+   j$screenWidth <- j[2,16]   # get screen width
+   j$screenHeight <- j[2,17]  # get screen height
+   j$OS <- j[2,18]            # get operating system
+   j$OS_lang <- j[2,19]       # get operating system language
+   j$calibration <- j[2,20]   # get calibration
+   j$ip <- j[2,21]            # get IP address
+   j$code <- j[2,22]          # get completion code
 
-   j <- j[-c(1:3),]
+   j <- j[-c(1:4),]
    d <- rbind(d,j)
    }
 
-colnames(d) <- c(get(files[1])[3,],colnames(d[,tail(colnames(d),n=19)]))
+colnames(d) <- c(get(files[1])[4,],colnames(d[,tail(colnames(d),n=21)]))
 
 setwd("../")
 write.csv(d,file="TestData.csv", row.names=F)
